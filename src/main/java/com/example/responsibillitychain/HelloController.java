34fa13a2a750278f873d53e9b886d9486d66645e
Gatnaron <oganyan.ivan2016@yandex.ru>
2024-03-01 17:54:52 +0300

    a2

--- src/main/java/com/example/responsibillitychain/HelloController.java
+++ src/main/java/com/example/responsibillitychain/HelloController.java
@@ -6,6 +6,7 @@ import com.example.responsibillitychain.Models.Diamond;
 import com.example.responsibillitychain.Models.Redstone;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
@@ -16,24 +17,30 @@ public class HelloController implements EventHandler<KeyEvent> {
     private Pane mainPane;
     @FXML
     private VBox mainWindow;
+    @FXML
+    public Label labelD;
+    @FXML
+    public Label labelR;
+    @FXML
+    public Label labelG;
     GameEngine game;
 
     public void initialize()
     {
         mainPane.toFront();
         mainWindow.setOnKeyPressed(this::handle);
-        game = new GameEngine(mainPane);
+        game = new GameEngine(mainPane, labelD, labelR, labelG);
         game.objects.add(new Redstone(8,8));
         game.objects.add(new Redstone(5,3));
         game.objects.add(new Redstone(8,0));
         game.objects.add(new Redstone(0,1));
         game.objects.add(new Gold(7,1));
-        game.objects.add(new Gold(6,6));
+        game.objects.add(new Gold(5,6));
         game.objects.add(new Gold(1,4));
         game.objects.add(new Diamond(10,1));
         game.objects.add(new Diamond(5,7));
 
-        game.GameMove(mainPane);
+        game.GameMove(mainPane, labelD, labelR, labelG);
     }
     @Override
     public void handle(KeyEvent e) {
@@ -46,6 +53,6 @@ public class HelloController implements EventHandler<KeyEvent> {
         if (e.getCode() == KeyCode.D)
             game.PlayerMove(1, 0);
 
-        game.GameMove(mainPane);
+        game.GameMove(mainPane, labelD, labelR, labelG);
     }
 }
--- src/main/java/com/example/responsibillitychain/Logic/GameEngine.java
+++ src/main/java/com/example/responsibillitychain/Logic/GameEngine.java
@@ -4,6 +4,7 @@ import com.example.responsibillitychain.HelloApplication;
 import com.example.responsibillitychain.Models.EnterObject;
 import com.example.responsibillitychain.Models.Player;
 import com.example.responsibillitychain.Models.SceneObject;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
@@ -14,16 +15,22 @@ public class GameEngine {
     Pane pane;
     ActionChain chain;
 
+    Label labelD;
+    Label labelR;
+    Label labelG;
     public final int maxX = 12, maxY = 10;
     public List<EnterObject> objects = new ArrayList<EnterObject>();
-    public GameEngine(Pane pane)
+    public GameEngine(Pane pane, Label labelD, Label labelR, Label labelG)
     {
         chain = new ActionChain();
         player = new Player();
         player.SetCords(6,6);
 
         this.pane = pane;
-        GameMove(this.pane);
+        this.labelD = labelD;
+        this.labelR = labelR;
+        this.labelG = labelG;
+        GameMove(this.pane, this.labelD, this.labelR, this.labelG);
     }
 
     public Player getPlayer() {
@@ -38,7 +45,7 @@ public class GameEngine {
             pane.getChildren().add(objects.get(i).asObj());
         }
     }
-    public void GameMove(Pane pane)
+    public void GameMove(Pane pane, Label labelD, Label labelR, Label labelG)
     {
 
         for (int i = 0; i < objects.size(); i++) {
@@ -49,9 +56,9 @@ public class GameEngine {
         }
         DrawScene(pane);
 
-        System.out.println("Редстоун: " + player.Redstone);
-        System.out.println("Золото: " + player.Gold);
-        System.out.println("Алмазы: " + player.Diamond);
+        labelD.setText("Алмазы: " + player.Diamond);
+        labelR.setText("Редстоун: " + player.Redstone);
+        labelG.setText("Золото: " + player.Gold);
         System.out.println("\n\n");
     }
     public boolean CheckCords(int X, int Y)
